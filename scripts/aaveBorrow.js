48e68fba7edba0b69936b97ddf3ee196921e5e23
marouenebg <marwenbg123@gmail.com>
2023-04-29 19:28:11 +0100

    Initializing the project

---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,12 @@
+//aave consider everything as an ERC20 token
+
+async function main() {
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
