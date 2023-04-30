0bb8342eae9ba510b7a58f9e58e5f227669cbb65
marouenebg <marwenbg123@gmail.com>
2023-04-30 22:34:22 +0100

    IWeth.sol

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,5 +1,5 @@
 //aave consider everything as an ERC20 token
-
+//but ETH in itself is not an ERC20 token, we should use WETH which is Eth but in a format of an ERC20 token
 async function main() {
 }
 
---
+++ scripts/getWeth.js
@@ -0,0 +1,12 @@
+/*const { getNamedAccounts } = require("hardhat")
+
+async function getWeth() {
+
+//In order to interact with a contract, we need an account
+const { deployer } = await getNamedAccounts()
+//call the deposit function on the WETH contract
+// we need the abi, contract address 
+}
+
+module.exports = {getWeth}
+*/
