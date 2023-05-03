846d3d1c082c9f9463f68711bab1466e84fedd60
marouenebg <marwenbg123@gmail.com>
2023-05-03 22:52:48 +0100

    Borrowing and repaying the TOkEN DAI

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -26,8 +26,42 @@ await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
 //how much can we borrow 
 let {availableBorrowsETH, totalDebtETH} = await getBorrowUserData(lendingPool, deployer)
 
+
+const DaiPrice = await getDai() 
+const AmounDaiToBorrow = availableBorrowsETH.toString() * 0.95 * (1 / DaiPrice.toNumber())
+console.log(`Amount of DAI you can borrow is ${AmounDaiToBorrow}`)
+const AmounDaiToBorrowWei = ethers.utils.parseEther(AmounDaiToBorrow.toString())
+
+daiAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+await borrowDai(daiAddress, lendingPool, AmounDaiToBorrowWei, deployer)
+
+//Repay
+await repay(daiAddress, lendingPool, AmounDaiToBorrowWei, deployer)
+
 }
 
+
+
+
+async function repay(daiAddress,lendingPool, AmounDaiToBorrowWei, account) {
+    await approveERC20(daiAddress, lendingPool.address, AmounDaiToBorrowWei, account)
+    const repaytx = await lendingPool.repay(daiAddress, AmounDaiToBorrowWei,0, account )
+    await repaytx.wait(1)
+    console.log ("repayed")
+    }
+
+async function borrowDai(daiAddress, lendingPool, AmounDaiToBorrowWei, account) {
+const borrowtx = await lendingPool.borrow(daiAddress, AmounDaiToBorrowWei, 1, 0, account)
+await borrowtx.wait(1)
+console.log("You have borrowed")
+}
+
+async function  getDai() {
+    const DaiEthPriceFeed = await ethers.getContractAt("AggregatorV3Interface", "0x773616E4d11A78F511299002da57A0a94577F1f4")   
+    const price = (await DaiEthPriceFeed.latestRoundData())[1]
+    console.log(`The DAI/ETH price is ${price.toString()}`)
+    return price
+}
 
 async function getBorrowUserData(lendingPool, account) {
     const {totalCollateralETH, totalDebtETH, availableBorrowsETH}  = await lendingPool.getUserAccountData(account)   
